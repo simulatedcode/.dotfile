efe2f12c8cae8e6ea5669864a413c561ceaaf3da
simulatedcode <simulasi.studio@gmail.com>
2022-12-13 10:23:31 +0700

    ui: winbar_statusline

--- lua/loskepetos/keymaps.lua
+++ lua/loskepetos/keymaps.lua
@@ -1,43 +1,47 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
-keymap.set('n', 'x', '"_x')
+keymap.set("n", "x", '"_x')
 
 -- Alpha
-keymap.set('n', '<F2>', ':Alpha<CR>', opts)
+keymap.set("n", "<F2>", ":Alpha<CR>", opts)
 
 -- Increment/decrement
-keymap.set('n', '+', '<C-a>')
-keymap.set('n', '-', '<C-x>')
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
 
 -- Delete a word backwards
-keymap.set('n', 'dw', 'vb"_d')
+keymap.set("n", "dw", 'vb"_d')
 
 -- Select all
-keymap.set('n', '<C-a>', 'gg<S-v>G')
+keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
 -- New tab
-keymap.set('n', 'te', ':tabedit')
+keymap.set("n", "te", ":tabedit")
 
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w')
-keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
+keymap.set("n", "ss", ":split<Return><C-w>w")
+keymap.set("n", "sv", ":vsplit<Return><C-w>w")
+
+-- Navigate buffers
+keymap.set("n", "<Tab>", ":bnext<CR>", opts)
+keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
 
 -- Move window
-keymap.set('n', '<Space>', '<C-w>w')
-keymap.set('', 'sh', '<C-w>h')
-keymap.set('', 'sk', '<C-w>k')
-keymap.set('', 'sj', '<C-w>j')
-keymap.set('', 'sl', '<C-w>l')
+keymap.set("n", "<Space>", "<C-w>w")
+keymap.set("", "sh", "<C-w>h")
+keymap.set("", "sk", "<C-w>k")
+keymap.set("", "sj", "<C-w>j")
+keymap.set("", "sl", "<C-w>l")
 
 -- Resize window
-keymap.set('n', '<C-w><left>', '<C-w><')
-keymap.set('n', '<C-w><right>', '<C-w>>')
-keymap.set('n', '<C-w><up>', '<C-w>+')
-keymap.set('n', '<C-w><down>', '<C-w>-')
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
 
 -- Visual --
 -- Stay in indent mode
--- lua/loskepetos/utilities/bufferline.lua
+++ lua/loskepetos/utilities/bufferline.lua
@@ -1,31 +1,35 @@
-require("bufferline").setup({
-  options = {
-    mode = "buffers",
-    separator_style = "slant",
-    always_show_bufferline = true,
-    show_buffer_close_icons = true,
-    show_close_icon = false,
-    buffer_modified_icon = "",
-    color_icons = false,
-    diagnostics = "nvim_lsp",
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      local s = " "
-      for e, n in pairs(diagnostics_dict) do
-        local sym = e == "error" and " " or (e == "warning" and " " or "")
-        s = s .. n .. sym
-      end
-      return s
-    end,
-    offsets = {
-      {
-        filetype = "NvimTree",
-        text = "File Explorer",
-        text_align = "left",
-        separator = true,
-      },
-    },
-  }
-})
+local status, bufferline = pcall(require, "bufferline")
+if not status then
+	return
+end
 
-vim.keymap.set("n", "<S-h>", "<Cmd>BufferLineCycleNext<CR>", {})
-vim.keymap.set("n", "<S-l>", "<Cmd>BufferLineCyclePrev<CR>", {})
+bufferline.setup({
+	options = {
+		mode = "tabs",
+		separator_style = "slant",
+		always_show_bufferline = false,
+		show_buffer_close_icons = false,
+		show_close_icon = false,
+		color_icons = true,
+	},
+	highlights = {
+		separator = {
+			fg = "#073642",
+			bg = "#002b36",
+		},
+		separator_selected = {
+			fg = "#073642",
+		},
+		background = {
+			fg = "#657b83",
+			bg = "#002b36",
+		},
+		buffer_selected = {
+			fg = "#fdf6e3",
+			bold = true,
+		},
+		fill = {
+			bg = "#073642",
+		},
+	},
+})
